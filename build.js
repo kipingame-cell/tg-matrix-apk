3590911dfce28fa37307a383691e40ae8f0235f2
kipingame-cell <kipin.game@gmail.com>
2026-08-09 22:07:04 +0300

    build: define global=window

--- build.js
+++ build.js
@@ -20,6 +20,6 @@ esbuild.build({
         constants: './src/empty.js'
     },
     plugins: [NodeGlobalsPolyfillPlugin({ process: true, buffer: true })],
-    define: { 'process.env.NODE_ENV': '"production"' },
+    define: { 'process.env.NODE_ENV': '"production"', 'global': 'window' },
     logLevel: 'info'
 }).catch(() => process.exit(1));
